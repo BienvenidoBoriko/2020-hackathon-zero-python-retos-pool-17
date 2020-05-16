b2f611a9460b483e8d74df7d082e021203d92e93
bienvenido <bienvenidoborico@gmail.com>
2020-05-16 17:22:16 +0200

    prueba kata1

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -3,21 +3,51 @@ from random import randint
 options = ["Piedra", "Papel", "Tijeras"]
 
 # El resultado de salida son las siguientes String
-#'Empate!'
-#'Ganaste!'
-#'Perdiste!'
-def quienGana(player, ai):
-    return ""
+# 'Empate!'
+# 'Ganaste!'
+# 'Perdiste!'
 
-# Entry Point
+
+def quienGana(player, ai):
+
+    player = player.lower()
+
+    ai = ai.lower()
+
+    if (player == 'piedra' and ai == 'papel'):
+        return 'Perdiste!'
+    elif(player == 'piedra' and ai == 'piedra'):
+        return 'Empate!'
+    elif(player == 'papel' and ai == 'papel'):
+        return 'Empate!'
+    elif(player == 'tijeras' and ai == 'tijeras'):
+        return 'Empate!'
+    elif(player == 'piedra' and ai == 'papel'):
+        return 'Perdiste!'
+    elif(player == 'papel' and ai == 'tijeras'):
+        return 'Perdiste!'
+    elif(player == 'tijeras' and ai == 'piedra'):
+        return 'Perdiste!'
+    elif(player == 'piedra' and ai == 'tijeras'):
+        return 'Ganaste!'
+    elif(player == 'papel' and ai == 'piedra'):
+        return 'Ganaste!'
+    elif(player == 'tijeras' and ai == 'papel'):
+        return 'Ganaste!'
+    else:
+        return 'obcion invalida'
+
+
 def Game():
-    #
-    #
-    
-    #
-    #
-    
-    winner = quienGana(player, ai)
+    global options
+
+    optionIa = options[randint(0, len(options) - 1)]
+
+    opcionUser = input("elige una opcion [Piedra - Papel - Tijera]: ")
+
+    winner = quienGana(opcionUser, optionIa)
 
     print(winner)
 
+
+Game()
