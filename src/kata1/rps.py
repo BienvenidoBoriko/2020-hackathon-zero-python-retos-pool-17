b4b6d5271baa981cc99ed545f3f65957ed88915c
bienvenido <bienvenidoborico@gmail.com>
2020-05-16 19:38:25 +0200

    acabada kata2

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -2,11 +2,6 @@ from random import randint
 
 options = ["Piedra", "Papel", "Tijeras"]
 
-# El resultado de salida son las siguientes String
-# 'Empate!'
-# 'Ganaste!'
-# 'Perdiste!'
-
 
 def quienGana(player, ai):
 
--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -2,12 +2,22 @@
 
 import random
 import string
+from random import randint
+
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
 
-    return ""
+    letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    letrasMinus = letras.lower()
+    numeros = "1234567890"
+    caracteresEspeciale = "|@#~€¬!·$%/&%(/)/¿?^*"
+
+    opciones = [letras, letrasMinus, numeros, caracteresEspeciale]
+    contraseña = ''
+    for i in range(passLen):
+        opcion = opciones[randint(0, len(opciones)-1)]
+        contraseña += opcion[randint(0, len(opcion)-1)]
+    return contraseña
+
+
+print(RandomPasswordGenerator(15))
